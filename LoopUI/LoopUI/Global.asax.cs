07abcfe3b7efe4bfca4d481e15a827282ee2521a
Alexandra Panaryna <sandra@scand.com>
2013-01-18 14:17:13 +0300

    Initial version of ui for Loop

---
+++ LoopUI/LoopUI/Global.asax.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace LoopUI
+{
+	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+	// visit http://go.microsoft.com/?LinkId=9394801
+
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routes.MapRoute(
+					"Default", // Route name
+					"{controller}/{action}/{id}", // URL with parameters
+					new { controller = "Login", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+			);
+
+		}
+
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+
+			RegisterGlobalFilters(GlobalFilters.Filters);
+			RegisterRoutes(RouteTable.Routes);
+		}
+
+		/*protected void Application_Error()
+		{
+			var exception = Server.GetLastError();
+			var httpException = exception as HttpException;
+			Response.Clear();
+			Server.ClearError();
+			var routeData = new RouteData();
+			routeData.Values["controller"] = "Error";
+			routeData.Values["action"] = "General";
+			routeData.Values["exception"] = exception;
+			Response.StatusCode = 500;
+			if (httpException != null)
+			{
+				Response.StatusCode = httpException.GetHttpCode();
+				switch (Response.StatusCode)
+				{
+					case 404:
+						routeData.Values["action"] = "Http404";
+						break;
+					case 500:
+						routeData.Values["action"] = "Http500";
+						routeData.Values["exception"] = exception;
+						break;
+				}
+			}
+
+			Response.TrySkipIisCustomErrors = true;
+			IController errorsController = new ErrorController();
+			HttpContextWrapper wrapper = new HttpContextWrapper(Context);
+			var rc = new RequestContext(wrapper, routeData);
+			errorsController.Execute(rc);
+		}*/
+	}
+}
