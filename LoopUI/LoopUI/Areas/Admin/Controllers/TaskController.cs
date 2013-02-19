af20e9a6ca76ea648975afa0c3591e95944ab9a7
Alexandra Panaryna <sandra@scand.com>
2013-02-19 20:32:33 +0200

    added first jqgrid added helper for tasks added checkbox editor for grid

--- LoopUI/LoopUI/Areas/Admin/Controllers/TaskController.cs
+++ LoopUI/LoopUI/Areas/Admin/Controllers/TaskController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using LoopUI.Models;
+using LoopUI.Helpers;
 
 namespace LoopUI.Areas.Admin.Controllers
 {
@@ -14,17 +15,23 @@ namespace LoopUI.Areas.Admin.Controllers
 
 		public ActionResult Index()
 		{
+			return View();
+		}
+
+		public ActionResult GetTasks()
+		{
 			List<Task> result = new List<Task>();
 			result.Add(new Task()
 			{
 				Id = 1,
 				Number = "IR-1010100",
 				Title = "Test",
-				Status = new TaskStatus() { Id = 1, Status = "Planning" },
-				IsActive = false
+				Status = new TaskStatus() { Id = 1, Status = "1:Planning" },
+				IsActive = true
 				//todo: add assignment here
 			});
-			return View(result);
+			//Json(JSonConverters.GetJsonForUsers(users, page, total, total / rows + 1), JsonRequestBehavior.AllowGet);
+			return Json(JSonConverters.GetJSonForTasks(result, 1, 1, 1),JsonRequestBehavior.AllowGet);
 		}
 	}
 }
---
+++ LoopUI/LoopUI/Controllers/Helpers/JSonConverters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LoopUI.Models;
+
+namespace LoopUI.Helpers
+{
+	internal class JSonConverters
+	{
+		internal static object GetJSonForTasks(IEnumerable<Task> tasks, int page, int count, int pageCount)
+		{
+			var result = new
+			{
+				total = pageCount,
+				page,
+				records = count,
+				rows = tasks.Select(x => new
+				{
+					id = x.Id,
+					cell = new[]
+					{
+							x.Number,
+							x.Title,
+							x.Status.Status,
+							x.IsActive.ToString()
+					}
+				})
+			};
+			return result;
+		}
+	}
+}
